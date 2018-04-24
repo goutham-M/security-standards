b7add6dcfa805cb895d25d52b5daff3dc62e6842
gomahesh <gomahesh@deloitte.com>
2018-04-24 15:37:28 +0530

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name securityStandardApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the securityStandardApp
+ */
+angular.module('securityStandardApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name securityStandardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the securityStandardApp
+ */
+angular.module('securityStandardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
